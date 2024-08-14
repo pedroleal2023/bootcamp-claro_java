4cd4f922a9c0b9366be36319e9bc74d78014c9ff
Pedro Miguel <rudaprod.oficial@gmail.com>
2024-08-14 15:24:02 -0300

    Conta do Banco - projeto Dio

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -16,26 +16,32 @@ public class ContaTerminal {
         String agencia;
         String nomeCliente;
         double saldo; 
-        
-        System.out.println("Por favor, digite o numero da conta: ");
+        double deposito;
+
+        System.out.print("Por favor, digite o numero da conta: ");
         numero = sc.nextInt();
-        
+        System.out.println();
         System.out.print("Por favor, digite o numero da agência: ");
         agencia = sc.next();
-
+        System.out.println();  
         System.out.print("Por favor, digite o seu nome: ");
         nomeCliente = sc.next();
-        
+        System.out.println();
+        System.out.print("Valor para deposito: ");
+        deposito = sc.nextDouble();
+
+        double soma = 0;
         saldo = 237.48;
 
+          soma = saldo + deposito;
 
-        if (saldo < 100.0){
-            System.out.println("SALDO INSUFICIENTE.");
+        if (soma < 300.0){
+            System.out.println("SALDO INSUFICIENTE PARA SAQUE.");
         } else {
-            System.out.println("SEU SALDO É: " + saldo);
+            System.out.println("SEU SALDO É: " + soma);
         }
 
-        System.out.println("Olá " + nomeCliente +", obrigado por criar uma conta em nosso banco, sua agência é " + agencia +", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.");
+        System.out.println("Caro Senhor(a) " + nomeCliente +", esses são os dados de sua conta em nosso banco. Sua agência é: " + agencia +", conta: " + numero + " seu saldo: " + saldo);
     
        sc.close();
     
